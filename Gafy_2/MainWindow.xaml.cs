a196a38c4a326da90da219db0d80b7534f399f18
Paula Palcowska <palcowskapaula@gmail.com>
2017-04-23 13:06:30 +0200

    Ciąg graficzny - 2.1

--- Gafy_2/MainWindow.xaml.cs
+++ Gafy_2/MainWindow.xaml.cs
@@ -24,5 +24,77 @@ namespace Gafy_2
         {
             InitializeComponent();
         }
+
+        //Tutaj przekazujemy ciąg znaków i sprawdzamy czy jest on graficzny
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Sequence.Text != "0")
+            {
+                Sequence.Background = Brushes.White;
+                string FromTheBox = Sequence.Text;
+                Sequence.Text = "";
+                string[] SeparetedFromTheBox = FromTheBox.Split(',');
+                int[] TabOfInt= Array.ConvertAll(SeparetedFromTheBox, i => int.Parse(i));
+                Array.Sort(TabOfInt);
+                //W tym miejscu mamy juz posortowana tablice intów
+                //Ta zmienna mówi nam czy ciąg jest grafuiczny czy nie jest
+                bool TrueOrFalse = false;
+                //znacznik konca petli
+                bool EndLoop = false;
+                //idneks ostatniego elementu tablicy
+                int max = TabOfInt.Length-1;
+                
+                //tu się dzieja rzeczy
+                while (EndLoop == false)
+                {
+                    //bierzemy ostani element i sprawdzamy czy nie jest on za duzy jak np. w ciagu 1,2,8 
+                    //bo wtedy nie mozemy go odjac i z miejsca taki ciag nie jest graficzny
+                    Array.Sort(TabOfInt);
+                    int value = TabOfInt[max];
+                    if (value > max)
+                    {
+                        TrueOrFalse = false;
+                        EndLoop = true;
+                    }
+                    //jesli sie zgadza wszytko to po kolei odejmujemy i ustawiamy value na 0
+                    else
+                    {
+                        for (int i = max - 1; i >= max - value; i--)
+                        {
+                            TabOfInt[i]--;
+                        }
+                        TabOfInt[max] = 0;
+                        //sortowanie
+                        Array.Sort(TabOfInt);
+                        //jesli ostatnia, czyli najwieksza liczba, to 0, to znaczy ze jest koniec
+                        //tutaj zaznaczamy EndLoop na true (konczymy petle)
+                        //zakladamy tez ze mamy same 0 - wtedy ciag jest graficzny - czyli TrueOrFalse = true
+                        //potem w petli for sprawdzamy czy nie mamy jakiego (-1) - ciag nie jest graficzny
+                        if (TabOfInt[max] == 0)
+                        {
+                            EndLoop = true;
+                            TrueOrFalse = true;
+                            for (int i = 0; i <= max; i++)
+                            {
+                                if (TabOfInt[i] < 0) TrueOrFalse = false;
+                            }
+                        }
+                    }
+                    
+                }
+
+                //wyswietlamy odpowiedni komunikat w miejscu, gdzie byl ciag
+                string OutPut;
+                if (TrueOrFalse == true)
+                    OutPut = "Tak";
+                else
+                    OutPut = "Nie";
+                Sequence.Text = OutPut;
+            }
+            else
+            {
+                Sequence.Background = Brushes.OrangeRed;
+            }
+        }
     }
 }
